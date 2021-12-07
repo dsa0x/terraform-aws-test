9224d9972cfd1b29c4cc9369e93661baf85785aa
Samsondeen Dare <samsondeen.dare@hashicorp.com>
2021-12-07 11:47:47 +0100

    add tf files

--- main.tf
+++ main.tf
@@ -30,12 +30,22 @@ resource "google_storage_bucket" "static-site" {
     response_header = ["*"]
     max_age_seconds = 3600
   }
+ versioning {
+    enabled = var.versioning
+  }
 
   dynamic "retention_policy" {
-    for_each = var.retention_policy == null ? [] : [var.retention_policy]
-    content {
-      is_locked        = var.retention_policy.is_locked
-      retention_period = var.retention_policy.retention_period
-    }
-  }
+    for_each = var.retention_policy == null ? [] : [var.retention_policy]
+    content {
+      is_locked        = var.retention_policy.is_locked
+      retention_period = var.retention_policy.retention_period
+    }
+  }
+
+  dynamic "encryption" {
+    for_each = var.encryption == null ? [] : [var.encryption]
+    content {
+      default_kms_key_name = var.encryption.default_kms_key_name
+    }
+  }
 }
--- variables.tf
+++ variables.tf
@@ -1,8 +1,17 @@
 variable "retention_policy" {
-  description = "Configuration of the bucket's data retention policy for how long objects in the bucket should be retained."
-  type = object({
-    is_locked        = bool
-    retention_period = number
-  })
-  default = null
+  description = "Configuration of the bucket's data retention policy for how long objects in the bucket should be retained."
+  type = object({
+    is_locked        = bool
+    retention_period = number
+  })
+  default = null
 }
+
+variable "encryption" {
+  description = "A Cloud KMS key that will be used to encrypt objects inserted into this bucket"
+  type = object({
+    default_kms_key_name = string
+  })
+  default = null
+}
+
